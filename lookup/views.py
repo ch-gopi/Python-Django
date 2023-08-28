fb389aa765981fa1fb2ed9889b0729b640529ddf
Gopi Chowdam <ch.gopi7893@gmail.com>
2023-08-28 20:43:05 +0530

    added content to views.py

--- lookup/views.py
+++ lookup/views.py
@@ -3,7 +3,15 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request,'home.html', {} )
+    import json
+    import requests
+    api_request = requests.get("http://api.weatherapi.com/v1/current.json?key=fdd6f6b779b24469970122029232808&q=hyderabad&aqi=no")
+    try:
+        api=json.loads(api_request.content)
+    except Exception as e:
+        api= "Error"
+
+    return render(request,'home.html', {'api':api} )
 def about(request):
     return render(request,'about.html', {} )
 
