e511d44634c289716de71d22a5055b104f2525d2
ch-gopi <58359910+ch-gopi@users.noreply.github.com>
2023-09-19 08:34:33 +0530

    Add files via upload

--- Projects/Django API/__init__.py
+++ Projects/Django API/__init__.py
@@ -1,31 +1,15 @@
-"""distutils.command
+"""distutils
 
-Package containing implementation of all the standard Distutils
-commands."""
+The main package for the Python Module Distribution Utilities.  Normally
+used from a setup script as
 
-__all__ = ['build',
-           'build_py',
-           'build_ext',
-           'build_clib',
-           'build_scripts',
-           'clean',
-           'install',
-           'install_lib',
-           'install_headers',
-           'install_scripts',
-           'install_data',
-           'sdist',
-           'register',
-           'bdist',
-           'bdist_dumb',
-           'bdist_rpm',
-           'bdist_wininst',
-           'check',
-           'upload',
-           # These two are reserved for future use:
-           #'bdist_sdux',
-           #'bdist_pkgtool',
-           # Note:
-           # bdist_packager is not included because it only provides
-           # an abstract base class
-          ]
+   from distutils.core import setup
+
+   setup (...)
+"""
+
+import sys
+
+__version__ = sys.version[:sys.version.index(' ')]
+
+local = True
